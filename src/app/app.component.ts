6b04e681778c1af4caeb6b6b72634ea1766b8284
pottasreekanthyadav <pottasreekanthyadav@gmail.com>
2019-11-15 20:16:27 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component ,OnInit} from '@angular/core';
 import {of,from} from "rxjs";
-import { map,tap} from 'rxjs/operators';
+import { map,tap,take} from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -9,13 +9,22 @@ import { map,tap} from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   ngOnInit(){
-    of(2,4,6,8).pipe(
-      map(i=>i*2)
-    ).subscribe(console.log)
+    // of(2,4,6,8).pipe(
+    //   map(i=>i*2)
+    // ).subscribe(console.log)
     
-    of(2,4,6,8).pipe(
-      tap(i=>console.log(i))
-    )
+    // of(2,4,6,8).pipe(
+     
+    //   tap(i=>console.log(i)),
+    //     map(i=>i*2),
+    //      tap(i=>console.log(i)),
+    //        map(i=>i-3),
+    //         tap(i=>console.log(i))
+
+    // ).subscribe()
+    // of(2,4,6,9).pipe(
+    //   take(2)
+    // ).subscribe(console.log)
   }
   name = 'Angular';
 }
